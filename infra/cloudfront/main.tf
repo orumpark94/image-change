cd1b29d6493760a1d00cfc0b587c726aa8f17216
orumpark94 <nowd940@gmail.com>
2025-07-19 09:42:46 +0900

    외부 요청 개선

--- infra/cloudfront/main.tf
+++ infra/cloudfront/main.tf
@@ -12,22 +12,25 @@ resource "aws_cloudfront_response_headers_policy" "secure_policy" {
 
     content_security_policy {
       override = true
-      content_security_policy = "default-src 'none'; img-src 'self'"
+      content_security_policy = <<EOF
+default-src 'self';
+script-src 'self' 'unsafe-inline';
+style-src 'self' 'unsafe-inline';
+img-src 'self' data:;
+connect-src 'self' https:;
+EOF
     }
-
   }
-
 }
-# ✅ 수정됨: 버킷 이름을 문자열로 직접 조합하여 참조
+
 resource "aws_cloudfront_distribution" "this" {
   origin {
     domain_name = "${var.s3_bucket_name}.s3.${var.region}.amazonaws.com"
     origin_id   = "s3-origin"
 
     s3_origin_config {
-  origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path  # ✅ 직접 생성한 OAI 참조
-}
-
+      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
@@ -49,7 +52,7 @@ resource "aws_cloudfront_distribution" "this" {
     response_headers_policy_id = aws_cloudfront_response_headers_policy.secure_policy.id
   }
 
-    viewer_certificate {
+  viewer_certificate {
     cloudfront_default_certificate = true
     minimum_protocol_version       = "TLSv1.2_2021"
   }
