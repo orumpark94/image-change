5f11e440d52d1b316289448bc98049c50c27eb6a
orumpark94 <nowd940@gmail.com>
2025-07-19 13:36:54 +0900

    test

--- infra/api-gateway/main.tf
+++ infra/api-gateway/main.tf
@@ -69,13 +69,7 @@ resource "aws_api_gateway_integration" "post_lambda_presign" {
   uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${var.lambda_presign_function_name}/invocations"
 }
 
-resource "aws_lambda_permission" "api_gw_invoke_presign_post" {
-  statement_id  = "AllowAPIGatewayInvokePresignPost"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_presign_function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/POST/presign"
-}
+
 
 ############################
 # CORS 설정 (GET 응답)
