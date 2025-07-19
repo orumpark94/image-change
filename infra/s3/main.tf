c94863e1584b8dcde2b708d2250f30aa0b7f22a9
orumpark94 <nowd940@gmail.com>
2025-07-19 16:03:41 +0900

    cors 설정

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_cors_configuration" "cors" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET"]
+    allowed_methods = ["GET","PUT"]
     allowed_origins = ["https://${var.cloudfront_domain_name}"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
@@ -82,27 +82,6 @@ resource "aws_s3_bucket_policy" "allow_cf_read" {
     ]
   })
 }
-
-#resource "aws_lambda_permission" "allow_s3_to_invoke_resize" {
-#  statement_id  = "AllowS3InvokeResizeLambda"
-#  action        = "lambda:InvokeFunction"
-#  function_name = var.resize_lambda_name
-#  principal     = "s3.amazonaws.com"
-#  source_arn    = aws_s3_bucket.output_bucket.arn
-#}
-
-#resource "aws_s3_bucket_notification" "s3_to_resize_lambda" {
-#  bucket = aws_s3_bucket.output_bucket.id
-
-#  lambda_function {
-#    lambda_function_arn = var.resize_lambda_arn
-#    events              = ["s3:ObjectCreated:*"]
-#    filter_prefix       = "uploads/"   # 사용자가 업로드할 디렉토리
-#    filter_suffix       = ".jpg"       # 확장자 (필요 시 여러 번 정의 가능)
-#  }
-
-#  depends_on = [aws_lambda_permission.allow_s3_to_invoke_resize]
-#}
 
 
 terraform {
